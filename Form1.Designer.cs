fc231000fb005292fc9858e7819825580f06322c
nghiadiyno <nghianguyennd28@gmail.com>
2023-09-14 12:30:40 +0700

    FormBanHang

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace FormBanHang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Mã 1",
+            "Giày",
+            "1000"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Mã 2",
+            "áo",
+            "100",
+            ""}, -1);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Mã 3",
+            "quần",
+            "378",
+            ""}, -1);
+            this.lsvBanHang = new System.Windows.Forms.ListView();
+            this.txtMaHang = new System.Windows.Forms.TextBox();
+            this.txtTenHang = new System.Windows.Forms.TextBox();
+            this.txtSL = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLamMoi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lsvBanHang
+            // 
+            this.lsvBanHang.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lsvBanHang.FullRowSelect = true;
+            this.lsvBanHang.GridLines = true;
+            this.lsvBanHang.HideSelection = false;
+            this.lsvBanHang.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.lsvBanHang.Location = new System.Drawing.Point(12, 56);
+            this.lsvBanHang.Name = "lsvBanHang";
+            this.lsvBanHang.Size = new System.Drawing.Size(555, 271);
+            this.lsvBanHang.TabIndex = 0;
+            this.lsvBanHang.UseCompatibleStateImageBehavior = false;
+            this.lsvBanHang.View = System.Windows.Forms.View.Details;
+            this.lsvBanHang.SelectedIndexChanged += new System.EventHandler(this.lsvBanHang_SelectedIndexChanged);
+            // 
+            // txtMaHang
+            // 
+            this.txtMaHang.Location = new System.Drawing.Point(668, 72);
+            this.txtMaHang.Name = "txtMaHang";
+            this.txtMaHang.Size = new System.Drawing.Size(188, 22);
+            this.txtMaHang.TabIndex = 1;
+            // 
+            // txtTenHang
+            // 
+            this.txtTenHang.Location = new System.Drawing.Point(668, 107);
+            this.txtTenHang.Multiline = true;
+            this.txtTenHang.Name = "txtTenHang";
+            this.txtTenHang.Size = new System.Drawing.Size(188, 36);
+            this.txtTenHang.TabIndex = 1;
+            // 
+            // txtSL
+            // 
+            this.txtSL.Location = new System.Drawing.Point(668, 178);
+            this.txtSL.Name = "txtSL";
+            this.txtSL.Size = new System.Drawing.Size(188, 22);
+            this.txtSL.TabIndex = 1;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(626, 249);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(781, 249);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(781, 304);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã hàng";
+            this.columnHeader1.Width = 110;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên hàng";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 286;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Số lượng";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 253;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(573, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tên hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(573, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mã hàng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(573, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Số lượng";
+            // 
+            // btnLamMoi
+            // 
+            this.btnLamMoi.Location = new System.Drawing.Point(626, 304);
+            this.btnLamMoi.Name = "btnLamMoi";
+            this.btnLamMoi.Size = new System.Drawing.Size(75, 23);
+            this.btnLamMoi.TabIndex = 2;
+            this.btnLamMoi.Text = "Làm mới";
+            this.btnLamMoi.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 372);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLamMoi);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtSL);
+            this.Controls.Add(this.txtTenHang);
+            this.Controls.Add(this.txtMaHang);
+            this.Controls.Add(this.lsvBanHang);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lsvBanHang;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox txtMaHang;
+        private System.Windows.Forms.TextBox txtTenHang;
+        private System.Windows.Forms.TextBox txtSL;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLamMoi;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormBanHang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {//thêm tt
+            if (check() == true)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = txtMaHang.Text;
+                item.SubItems.Add(txtTenHang.Text);
+                item.SubItems.Add(txtSL.Text);
+                lsvBanHang.Items.Add(item);
+            }
+            else 
+            {
+                MessageBox.Show("Nhập thiếu dữ liệu hoặc trùng mã !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } 
+            
+        }
+        public bool check()
+        {
+            if (txtSL.Text.Trim() == "" ||
+                txtMaHang.Text.Trim() == "" ||
+                txtTenHang.Text.Trim() == "")
+                return false;
+            foreach (ListViewItem x in lsvBanHang.Items)
+                if (string.Compare(x.Text, txtMaHang.Text, true) == 0)
+                    return false;
+   
+            return true;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            
+            if (lsvBanHang.SelectedItems.Count > 0)
+            {
+                if(MessageBox.Show("Bạn có muốn sửa ?","Thông báo",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    int i = lsvBanHang.SelectedItems[0].Index;
+                    if (i >= 0)
+                    {
+                       // if(check() == true)
+                        // {
+                        lsvBanHang.Items[i].Text = txtMaHang.Text;
+                        //  }
+                        // else
+                        // {
+                        //     MessageBox.Show("Mã bị trùng !", "Thông báo",MessageBoxButtons.OKCancel ,MessageBoxIcon.Warning);
+                        //  }
+                        lsvBanHang.Items[i].SubItems[1].Text = txtTenHang.Text;
+                        lsvBanHang.Items[i].SubItems[2].Text = txtSL.Text;
+                    }
+                }
+                
+            }
+        }
+
+        private void lsvBanHang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lsvBanHang.SelectedItems.Count > 0)
+            {
+                int i = lsvBanHang.SelectedItems[0].Index;
+                if (i >= 0)
+                {
+                    txtMaHang.Text = lsvBanHang.Items[i].Text;
+                    txtTenHang.Text = lsvBanHang.Items[i].SubItems[1].Text;
+                    txtSL.Text = lsvBanHang.Items[i].SubItems[2].Text;
+                }
+            }
+            
+            
+        }
+        public int Tong()
+        {
+            int sum = 0;
+            foreach (ListViewItem x in lsvBanHang.Items)
+            {
+                sum += int.Parse(x.SubItems[2].Text);
+            }
+            return sum;
+
+        }
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Tổng số lượng: {Tong().ToString()}");
+            if(lsvBanHang.SelectedItems.Count > 0)
+            {
+                int i = lsvBanHang.SelectedIndices[0];
+                if (MessageBox.Show("Xác nhận xóa ?","Thông báo",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    lsvBanHang.Items.RemoveAt(i);
+                }
+                
+            }
+        }
+    }
+}
